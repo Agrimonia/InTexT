80024f1b8800b53e9afe64b320810dd9c4e817a5
Yongcong Lin <646677472@qq.com>
2018-11-08 22:00:56 +0800

    Inital commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+const App = () => (<div >hello world</div>)
+
+const HTMLContainer = document.getElementById('container')
+
+ReactDOM.render(<App />, HTMLContainer);
