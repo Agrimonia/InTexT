14f210f670ef2df180dc417cf8a8477cc55ff1a1
Yongcong Lin <646677472@qq.com>
2018-11-13 23:36:06 +0800

    [WIP] prepare for backend

---
+++ src/components/sider.jsx
@@ -0,0 +1,21 @@
+import React from "React";
+
+export default class Sider extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="sider">
+        <div>
+          {/* icon-back */}
+          {/* icon-document */}
+        </div>
+        {/* menu-icon-new */}
+        {/* menu-icon-export */}
+      </div>
+    );
+  }
+}
